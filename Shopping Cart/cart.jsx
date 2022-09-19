6bc386bec012561ff1058af04d5d5c2deff17848
Matt Solomon <103535324+msolomon93@users.noreply.github.com>
2022-09-19 14:54:31 -0400

    Update cart.jsx

--- Shopping Cart/cart.jsx
+++ Shopping Cart/cart.jsx
@@ -1,9 +1,9 @@
 // simulate getting products from DataBase
 const products = [
-    { name: "Apples_:", country: "Italy", cost: 3, instock: 10 },
-    { name: "Oranges:", country: "Spain", cost: 4, instock: 3 },
-    { name: "Beans__:", country: "USA", cost: 2, instock: 5 },
-    { name: "Cabbage:", country: "USA", cost: 1, instock: 8 },
+    { name: "Apples :", country: "Italy", cost: 3, instock: 10 },
+    { name: "Oranges :", country: "Spain", cost: 4, instock: 3 },
+    { name: "Beans :", country: "USA", cost: 2, instock: 5 },
+    { name: "Cabbage :", country: "USA", cost: 1, instock: 8 },
   ];
   //=========Cart=============
   const Cart = (props) => {
